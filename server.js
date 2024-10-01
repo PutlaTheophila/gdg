c4c4fec7aabb22e78edf3779ba2dc513905d227e
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-01 15:54:17 +0530

    hello

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import mongoose  from 'mongoose';
 import app from "./app.js"
-export const  port = 9090;
+import dotenv from 'dotenv'
+dotenv.config()
+export const  port = process.env.PORT||6767;
 
-mongoose.connect('mongodb+srv://putlatheophila123:O3UJE3GOuBYnVzj5@cluster0.xy2080g.mongodb.net/cineflex?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(`mongodb+srv://putlatheophila123:${process.env.MONGODB_SECRET}@cluster0.xy2080g.mongodb.net/cineflex?retryWrites=true&w=majority&appName=Cluster0`)
 .then((conn)=>{
     console.log('connected to database 2');
     app.listen( port , ()=>{
